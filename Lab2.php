d6822cf6d7e5cfc713ce418527e92c9acf27a9bb
c0dead <anastasiaromankina1609@gmail.com>
2022-10-13 22:30:09 +0200

    Ex. 2

--- Lab2.php
+++ Lab2.php
@@ -5,7 +5,22 @@ $very_bad_unclear_name = "15 chicken wings";
 
 // Write your code here:
 $order = &$very_bad_unclear_name;
-$order .= " and french fries..";
+$order .= " and french fries";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+/* Ex. 2 */
+$number_int = 8;
+echo "\n$number_int";
+echo "\n";
+
+$number_float = 8.8;
+echo $number_float;
+
+echo "\n".($number_int + 4);
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+
+echo "\n".($last_month - $this_month);
